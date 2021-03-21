7d15ed445c86d478fd545dc9bd9de013f18a4f4d
Frenkli Kapri <kaprifrenkli@gmail.com>
2021-03-21 17:35:33 +0100

    https

--- TestApi/Startup.cs
+++ TestApi/Startup.cs
@@ -44,7 +44,7 @@ namespace TestApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestApi v1"));
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
